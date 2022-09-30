76f9b08ff99276cc605c76dacd3d929c0aefc312
Grégory BOISSINOT <gregory.boissinot@gmail.com>
2022-09-30 15:37:59 +0200

    Refactoring

--- src/main/java/fr/esgi/al/tps/classe1/tp4/Formatter.java
+++ src/main/java/fr/esgi/al/tps/classe1/tp4/Formatter.java
@@ -1,5 +1,6 @@
 package fr.esgi.al.tps.classe1.tp4;
 
+@FunctionalInterface
 interface Formatter {
     String format(String message);
 }
--- src/main/java/fr/esgi/al/tps/classe1/tp4/Main.java
+++ src/main/java/fr/esgi/al/tps/classe1/tp4/Main.java
@@ -3,7 +3,8 @@ package fr.esgi.al.tps.classe1.tp4;
 public class Main {
     public static void main(String[] args) {
         MyService myService =
-                new MyService(new ConsoleLogger(new NullFormatter()));
+                new MyService(
+                        new ConsoleLogger(message -> "[custom inline format] - " + message));
         myService.myMethod();
     }
 }
