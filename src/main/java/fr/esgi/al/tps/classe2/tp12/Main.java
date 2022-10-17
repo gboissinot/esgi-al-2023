0c6da17f9ed9d66ab12343adb1c70a6588d3fa97
Grégory BOISSINOT <gregory.boissinot@gmail.com>
2022-10-17 17:09:51 +0200

    Add tp12

---
+++ src/main/java/fr/esgi/al/tps/classe2/tp12/Main.java
@@ -0,0 +1,11 @@
+package fr.esgi.al.tps.classe2.tp12;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Colis colis = Colis.create(ColisNumber.of(123));
+        colis.markAsDelivered();
+        final History history = colis.history();
+        System.out.println(history);
+    }
+}
---
+++ src/main/java/fr/esgi/al/tps/classe2/tp12/Status.java
@@ -0,0 +1,7 @@
+package fr.esgi.al.tps.classe2.tp12;
+
+public enum Status {
+
+    IN_PREPARATION, DELIVERED, CANCELED;
+
+}
