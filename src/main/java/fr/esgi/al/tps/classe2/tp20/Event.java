a4d0f992f22a01dd7961d3262a2f5c5f6a1e10a7
Grégory BOISSINOT <gregory.boissinot@gmail.com>
2022-10-17 19:45:07 +0200

    Add tp20

---
+++ src/main/java/fr/esgi/al/tps/classe2/tp20/Event.java
@@ -0,0 +1,4 @@
+package fr.esgi.al.tps.classe2.tp20;
+
+public interface Event {
+}
---
+++ src/main/java/fr/esgi/al/tps/classe2/tp20/Player.java
@@ -0,0 +1,24 @@
+package fr.esgi.al.tps.classe2.tp20;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Player {
+
+    private final List<Event> recordedEvents;
+
+    public Player(Position intialPosition) {
+        this.recordedEvents = new ArrayList<>();
+        this.recordedEvents.add(new PlayerTookInitialPosition(intialPosition));
+    }
+
+    public void moveLeft(int steps) {
+        this.recordedEvents.add(new PlayerMoved(steps,0));
+    }
+
+    public List<Event> recordedEvents() {
+        return recordedEvents;
+    }
+
+}
