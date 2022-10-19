e9bdac7e92bef5201cfe214825af9f4679cb9530
Grégory BOISSINOT <gregory.boissinot@gmail.com>
2022-10-19 15:04:23 +0200

    add tp9

---
+++ src/main/java/fr/esgi/al/tps/classe1/tp9/Query.java
@@ -0,0 +1,30 @@
+package fr.esgi.al.tps.classe1.tp9;
+
+final class Query {
+
+    private final String select;
+    private final String from;
+    private final String where;
+    private final String orderBy;
+
+    Query(String select, String from, String where, String orderBy) {
+        this.select = select;
+        this.from = from;
+        this.where = where;
+        this.orderBy = orderBy;
+    }
+
+    @Override
+    public String toString() {
+        return "Query{" +
+                "select='" + select + '\'' +
+                ", from='" + from + '\'' +
+                ", where='" + where + '\'' +
+                ", orderBy='" + orderBy + '\'' +
+                '}';
+    }
+
+    public static QueryBuilder create() {
+        return new QueryBuilder();
+    }
+}
