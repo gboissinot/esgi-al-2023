82f0b94a4b74962d3b7e10eaee26ed2fb19d7328
Grégory BOISSINOT <gregory.boissinot@gmail.com>
2022-10-19 16:23:25 +0200

    Add tp11

---
+++ src/test/java/fr/esgi/al/tps/classe1/tp11/YearTest.java
@@ -0,0 +1,17 @@
+package fr.esgi.al.tps.classe1.tp11;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class YearTest {
+
+    @Test
+    public void next() {
+        Year year = new Year(2022);
+        year.next();
+        Assertions.assertEquals(new Year(2022), year);
+        Assertions.assertEquals(new Year(2023), year.next());
+        year = year.next();
+        Assertions.assertEquals(new Year(2023), year);
+    }
+}
